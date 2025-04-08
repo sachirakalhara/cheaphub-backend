29b621effdeac74cf5e3b09912aa8b102a781be0
Ishan <ishanpradeepkumara@gmail.com>
2025-04-08 23:36:24 +0530

    changes

--- app/Http/Resources/Category/CategoryResource.php
+++ app/Http/Resources/Category/CategoryResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\Category;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class CategoryResource extends JsonResource
 {
@@ -16,10 +17,14 @@ class CategoryResource extends JsonResource
      */
     public function toArray($request)
     {
+        $disk = Storage::disk('s3');
+        $image = $this->image ? $disk->url($this->image) : null;
+
         return [
             'id'=>$this->id,
             'name'=> $this->name,
-            'description'=>$this->description
+            'description'=>$this->description,
+            'image'=>$image
 
         ];
     }
