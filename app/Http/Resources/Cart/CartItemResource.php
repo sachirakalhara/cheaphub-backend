6701249a40d5ecd6e709f4b45a907eb93f05171b
Ishan <ishanpradeepkumara@gmail.com>
2025-04-10 09:39:41 +0530

    changes

--- app/Http/Resources/Cart/CartItemResource.php
+++ app/Http/Resources/Cart/CartItemResource.php
@@ -17,14 +17,19 @@ class CartItemResource extends JsonResource
      */
     public function toArray($request)
     {
+        $contributionProduct_image = null;
         if ($this->bulkProduct) {
             $this->bulkProduct->image = $this->bulkProduct->image ? Storage::disk('s3')->url($this->bulkProduct->image) : null;
+        }
+        if ($this->package) {
+            $contributionProduct_image = $this->package->subscription->contributionProduct ? Storage::disk('s3')->url($this->package->subscription->contributionProduct->image) : null;
         }
         return [
             'id'=>$this->id,
             'quantity'=> $this->quantity,
             'bulkProduct'=> $this->bulkProduct,
             'package'=> $this->package,
+            'contributionProduct_image' => $contributionProduct_image
         ];
     }
 
