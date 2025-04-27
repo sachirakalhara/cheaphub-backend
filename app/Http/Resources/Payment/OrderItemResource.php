fe664ea00efaa3c960c3f982400113e6837a90d9
Ishan <ishanpradeepkumara@gmail.com>
2025-04-27 09:11:20 +0530

    changes

--- app/Http/Resources/Payment/OrderItemResource.php
+++ app/Http/Resources/Payment/OrderItemResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Payment;
 
+use App\Http\Resources\Subscription\PackageResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class OrderItemResource extends JsonResource
@@ -22,9 +23,10 @@ class OrderItemResource extends JsonResource
             // 'order'=>OrderResource::collection($this->order),
             'quantity'=> $this->quantity,
             'bulkProduct'=> $this->bulkProduct,
-            'package'=> $this->package,
-            'created_at'=>$this->created_at
-            
+            'package'=> $this->package->with('subscription'),
+            'created_at'=>$this->created_at,
+            // 'package1'=>PackageResource::collection($this->order),
+
         ];
     }
 
