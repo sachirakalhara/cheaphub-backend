9346e1ade282604b3dd86391248cf852e08f5c0a
Ishan <ishanpradeepkumara@gmail.com>
2025-05-02 11:26:06 +0530

    changes

--- app/Http/Resources/Payment/OrderItemResource.php
+++ app/Http/Resources/Payment/OrderItemResource.php
@@ -6,7 +6,9 @@ use App\Http\Resources\Category\CategoryResource;
 use App\Http\Resources\Product\Bulk\BulkProductResource;
 use App\Http\Resources\Subscription\PackageResource;
 use App\Http\Resources\Subscription\SubscriptionResource;
+use App\Models\Subscription\Subscription;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
 class OrderItemResource extends JsonResource
@@ -24,34 +26,43 @@ class OrderItemResource extends JsonResource
 
         $disk = Storage::disk('s3');
 
+        $contributionProduct = optional(optional(optional($this->package)->subscription)->contributionProduct);
+        $image = $contributionProduct->image ? $disk->url($contributionProduct->image) : null;
+        $subscription = Subscription::find($this->package->subscription_id);
+        return [
+            'id' => $this->id,
+            'quantity' => $this->quantity,
+            'created_at' => $this->created_at,
 
-            $contributionProductImage = optional(
-                optional(
-                    optional($this->package)->subscription
-                )->contributionProduct
-            )->image;
-    
-            $imageUrl = $contributionProductImage ? $disk->url($contributionProductImage) : null;
-    
-            return [
-                'id' => $this->id,
-                'quantity' => $this->quantity,
-                'created_at' => $this->created_at,
-    
-                'bulk_product' => new BulkProductResource($this->bulkProduct),
-    
-                'package' => [
-                    'id' => optional($this->package)->id,
-                    'name' => optional($this->package)->name,
-                    'price' => optional($this->package)->price,
-                    'replace_count' => optional($this->package)->replace_count,
-                    'expiry_duration' => optional($this->package)->expiry_duration,
-                    'payment_method' => optional($this->package)->payment_method,
-                    'subscription_id' => optional(optional($this->package)->subscription)->id,
-                    'subscription_type' => optional(optional($this->package)->subscription)->type,
-                    'contribution_product_image' => $imageUrl,
-                ],
-            ];
+            'bulk_product' => new BulkProductResource($this->bulkProduct),
+
+            'package' => [
+                'id' => optional($this->package)->id,
+                'name' => optional($this->package)->name,
+                'price' => optional($this->package)->price,
+                'replace_count' => optional($this->package)->replace_count,
+                'expiry_duration' => optional($this->package)->expiry_duration,
+            ],
+
+            'subscription' => [
+                'id' => $subscription->id,
+                'name' => $subscription->name,
+                'serial' => $subscription->serial,
+                'available_serial_count' => $subscription->available_serial_count,
+                'gateway_fee' => $subscription->gateway_fee,
+            ],
+
+            'contribution_product' => [
+                'id' => $contributionProduct->id,
+                'name' => $contributionProduct->name,
+                'tag_id' => $contributionProduct->tag_id,
+                'description' => $contributionProduct->description,
+                'image' => $image,
+                'visibility' => $contributionProduct->visibility,
+                'service_info' => $contributionProduct->service_info,
+                'url' =>!empty(Auth::user()->id) ? "cheaphub.io/contribution/{$contributionProduct->id}/{$contributionProduct->name}" : null,
+            ],
+        ];
 
             // 'id'=>$this->id,
             // // 'order'=>OrderResource::collection($this->order),
