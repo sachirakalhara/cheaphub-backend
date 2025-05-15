0ae639be3ba567eac0be38e17e48c7a8ec64224f
Ishan <ishanpradeepkumara@gmail.com>
2025-05-15 20:35:11 +0530

    changes

--- app/Repositories/Payment/MarxPaymentRepository.php
+++ app/Repositories/Payment/MarxPaymentRepository.php
@@ -172,8 +172,8 @@ class MarxPaymentRepository implements MarxPaymentRepositoryInterface
 
         try {
            
-            $local_user_secret = '$2a$10$x1CAe9YuEz9G1X1ZQrTrLOJXFu2PSvrwLuBcWpgT2ecRAx5sxfOhW';
-            $marx_sandbox_url = 'https://payment.api.dev.marxpos.com/api/v4/ipg/orders';
+            $local_user_secret = 'OTYwZTVkYmEtMGFiZi00OGQ0LTk5ZDctNGM1YWY2NjhkNWUwXzkxMjY=';
+            $marx_sandbox_url = 'https://payment.v4.api.marx.lk/api/v4/ipg/orders';
             $response = Http::withHeaders([
                 'Content-Type' => 'application/json',
                 'merchant-api-key' => $local_user_secret,
@@ -226,8 +226,8 @@ class MarxPaymentRepository implements MarxPaymentRepositoryInterface
                     'message' => 'Missing required parameters.',
                 ], Response::HTTP_BAD_REQUEST);
             }
-            $production_url = 'https://payment.api.dev.marxpos.com/api/v4/ipg/orders';
-            $local_user_secret = '$2a$10$x1CAe9YuEz9G1X1ZQrTrLOJXFu2PSvrwLuBcWpgT2ecRAx5sxfOhW';
+            $production_url = 'https://payment.v4.api.marx.lk/api/v4/ipg/orders';
+            $local_user_secret = 'OTYwZTVkYmEtMGFiZi00OGQ0LTk5ZDctNGM1YWY2NjhkNWUwXzkxMjY=';
 
             $check_url = "{$production_url}/{$tr}";
             $response = Http::withHeaders([
